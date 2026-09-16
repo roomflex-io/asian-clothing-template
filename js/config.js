21dadd397039c8a05d4a0a883b5315a5e477a8f7
roomflex-io <zak@emanbuddy.app>
2026-09-16 16:19:30 +0100

    Point products at generated photo keys

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ window.SHOP = {
   gold: "#c4a574",
   ink: "#1a1410",
   cream: "#f6f1ea",
-  heroImg: "https://images.pexels.com/photos/20736200/pexels-photo-20736200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  heroImg: null,
   collections: {
     him:      { on: true,  label: "For him",     href: "shop.html?cat=him" },
     her:      { on: true,  label: "For her",     href: "shop.html?cat=her" },
@@ -26,29 +26,29 @@ window.SHOP = {
   }
 };
 
-const px = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=900`;
+window.pic = (key) => (window.PHOTOS && PHOTOS[key]) || "";
 
 window.PRODUCTS = [
-  { cat: "her", name: "Embroidered lawn suit", price: "£45", tag: "New", img: px(28144256) },
-  { cat: "her", name: "Silk kurti", price: "£38", tag: "", img: px(28210874) },
-  { cat: "him", name: "Cotton kurta", price: "£32", tag: "", img: px(8565796) },
-  { cat: "him", name: "Waistcoat set", price: "£65", tag: "", img: px(5416746) },
-  { cat: "summer", name: "Light chiffon set", price: "£42", tag: "Summer", img: px(28144256) },
-  { cat: "summer", name: "Printed lawn 3-piece", price: "£49", tag: "Summer", img: px(20736200) },
-  { cat: "winter", name: "Velvet shawl", price: "£55", tag: "Winter", img: px(33101418) },
-  { cat: "winter", name: "Pashmina wrap", price: "£70", tag: "Winter", img: px(31832653) },
-  { cat: "autumn", name: "Warm linen mix", price: "£40", tag: "Autumn", img: px(28210874) },
-  { cat: "lehenga", name: "Bridal lehenga", price: "£320", tag: "Bridal", img: px(29370686) },
-  { cat: "lehenga", name: "Reception lehenga", price: "£210", tag: "", img: px(20736200) },
-  { cat: "wedding", name: "Groom sherwani", price: "£280", tag: "Wedding", img: px(5416746) },
-  { cat: "wedding", name: "Guest formal", price: "£95", tag: "Wedding", img: px(8565796) },
-  { cat: "abayas", name: "Everyday abaya", price: "£48", tag: "", img: px(36229949) },
-  { cat: "abayas", name: "Open abaya + belt", price: "£62", tag: "New", img: px(35324621) },
-  { cat: "thobes", name: "Classic white thobe", price: "£36", tag: "", img: px(9813680) },
-  { cat: "thobes", name: "Embroidered collar thobe", price: "£52", tag: "", img: px(5416746) },
-  { cat: "perfumes", name: "Oud attar", price: "£28", tag: "", img: px(1961795) },
-  { cat: "perfumes", name: "Musk & rose", price: "£24", tag: "", img: px(965989) },
-  { cat: "accessories", name: "Jhumka earrings", price: "£18", tag: "", img: px(1454171) },
-  { cat: "accessories", name: "Clutch bag", price: "£22", tag: "", img: px(904350) },
-  { cat: "accessories", name: "Hijab pack", price: "£16", tag: "", img: px(7816722) }
+  { cat: "her", name: "Embroidered lawn suit", price: "£45", tag: "New", img: "summer" },
+  { cat: "her", name: "Silk kurti", price: "£38", tag: "", img: "summer" },
+  { cat: "him", name: "Cotton kurta", price: "£32", tag: "", img: "thobe" },
+  { cat: "him", name: "Waistcoat set", price: "£65", tag: "", img: "sherwani" },
+  { cat: "summer", name: "Light chiffon set", price: "£42", tag: "Summer", img: "summer" },
+  { cat: "summer", name: "Printed lawn 3-piece", price: "£49", tag: "Summer", img: "summer" },
+  { cat: "winter", name: "Velvet shawl", price: "£55", tag: "Winter", img: "winter" },
+  { cat: "winter", name: "Pashmina wrap", price: "£70", tag: "Winter", img: "winter" },
+  { cat: "autumn", name: "Warm linen mix", price: "£40", tag: "Autumn", img: "winter" },
+  { cat: "lehenga", name: "Bridal lehenga", price: "£320", tag: "Bridal", img: "lehenga" },
+  { cat: "lehenga", name: "Reception lehenga", price: "£210", tag: "", img: "lehenga" },
+  { cat: "wedding", name: "Groom sherwani", price: "£280", tag: "Wedding", img: "sherwani" },
+  { cat: "wedding", name: "Guest formal", price: "£95", tag: "Wedding", img: "sherwani" },
+  { cat: "abayas", name: "Everyday abaya", price: "£48", tag: "", img: "abaya" },
+  { cat: "abayas", name: "Open abaya + belt", price: "£62", tag: "New", img: "abaya" },
+  { cat: "thobes", name: "Classic white thobe", price: "£36", tag: "", img: "thobe" },
+  { cat: "thobes", name: "Embroidered collar thobe", price: "£52", tag: "", img: "thobe" },
+  { cat: "perfumes", name: "Oud attar", price: "£28", tag: "", img: "perfume" },
+  { cat: "perfumes", name: "Musk & rose", price: "£24", tag: "", img: "perfume" },
+  { cat: "accessories", name: "Jhumka earrings", price: "£18", tag: "", img: "accessories" },
+  { cat: "accessories", name: "Clutch bag", price: "£22", tag: "", img: "accessories" },
+  { cat: "accessories", name: "Hijab pack", price: "£16", tag: "", img: "abaya" }
 ];
