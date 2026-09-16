641ef136633c2296124a360186aee88cfb55933c
roomflex-io <zak@emanbuddy.app>
2026-09-16 16:22:18 +0100

    Add photo map pointing at local img files

---
+++ js/photos.js
@@ -0,0 +1,10 @@
+window.PHOTOS = {
+  lehenga: 'img/lehenga.jpg',
+  abaya: 'img/abaya.jpg',
+  thobe: 'img/thobe.jpg',
+  perfume: 'img/perfume.jpg',
+  sherwani: 'img/sherwani.jpg',
+  summer: 'img/summer.jpg',
+  winter: 'img/winter.jpg',
+  accessories: 'img/accessories.jpg'
+};
