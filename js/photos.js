8e7a06f43cf085208fef74e8a8fa82fe199edb25
roomflex-io <zak@emanbuddy.app>
2026-09-16 16:43:36 +0100

    Use local product photos instead of placeholder URLs

--- js/photos.js
+++ js/photos.js
@@ -1,10 +1,10 @@
 window.PHOTOS = {
-  lehenga: 'https://commons.wikimedia.org/wiki/Special:FilePath/Sikh_bride_01.jpg?width=900',
-  abaya: 'https://commons.wikimedia.org/wiki/Special:FilePath/Traditional_Green_Abayas_with_Gold_Thread_Embellishments.jpg?width=900',
-  thobe: 'https://commons.wikimedia.org/wiki/Special:FilePath/Thobe.jpg?width=900',
-  perfume: 'https://commons.wikimedia.org/wiki/Special:FilePath/Perfume_bottles.jpg?width=900',
-  sherwani: 'https://commons.wikimedia.org/wiki/Special:FilePath/Groom%27s_outfit.jpg?width=900',
-  summer: 'https://commons.wikimedia.org/wiki/Special:FilePath/Indian_Wedding_Designer_Lehengas.jpg?width=900',
-  winter: 'https://commons.wikimedia.org/wiki/Special:FilePath/Pashmina_shawl.jpg?width=900',
-  accessories: 'https://commons.wikimedia.org/wiki/Special:FilePath/Jhumka_earrings.jpg?width=900'
+  lehenga: 'img/lehenga.jpg',
+  abaya: 'img/abaya.jpg',
+  thobe: 'img/thobe.jpg',
+  perfume: 'img/perfume.jpg',
+  sherwani: 'img/sherwani.jpg',
+  summer: 'img/summer.jpg',
+  winter: 'img/winter.jpg',
+  accessories: 'img/accessories.jpg'
 };
